936fe03c767324029c226538b9b340078c52bb40
Rejoice <uahomorejoice@gmail.com>
2024-03-19 16:05:57 +0100

    fix

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 app.use(morgan("dev")); // configire morgan
 
 runFeedProductDaily()
