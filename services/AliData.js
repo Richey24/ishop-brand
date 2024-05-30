2b0d6571d10f1e8fd8b7df2e0c87b83786a26008
Rejoice <uahomorejoice@gmail.com>
2024-05-30 17:16:05 +0100

    fiz

--- services/AliData.js
+++ services/AliData.js
@@ -1,42 +1,42 @@
 const aliDatas = [
     {
         // clothes
+        // 5535
         category: 200000345,
         feed: "DS_Australia_fastdelivery 20231123",
         path: "DS_Australia_fastdelivery+20231123"
     },
     {
-        // hats, gloves, tie and cap
+        // hats, gloves, sunglasses, tie and cap
+        // 7119
         category: 200000297,
         feed: "DS_Australia_fastdelivery 20231123",
         path: "DS_Australia_fastdelivery+20231123"
     },
     {
         // jeans
+        // 2809
         category: 200000343,
         feed: "DS_Australia_fastdelivery 20231123",
         path: "DS_Australia_fastdelivery+20231123"
     },
     {
         // socks
+        // 3440
         category: 200574005,
         feed: "DS_Australia_fastdelivery 20231123",
         path: "DS_Australia_fastdelivery+20231123"
     },
     {
         // bags and wallet
+        // 2856
         category: 1524,
         feed: "DS_Australia_fastdelivery 20231123",
         path: "DS_Australia_fastdelivery+20231123"
     },
     {
-        // sunglasses
-        category: 200000297,
-        feed: "DS_Australia_fastdelivery 20231123",
-        path: "DS_Australia_fastdelivery+20231123"
-    },
-    {
         // first aid
+        // 1225
         category: 30,
         feed: "DS_Australia_fastdelivery 20231123",
         path: "DS_Australia_fastdelivery+20231123"
